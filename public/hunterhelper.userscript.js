95d156e9aa8ceef049826dd3a138364bf0a589f8
Pavlik <raventomsk@mail2000.ru>
2022-07-21 18:52:11 +0700

    + v0.3.3 hunter helper US loader + mini version

---
+++ public/hunterhelper.userscript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Twitter Following HighLight
+// @namespace    http://tampermonkey.net/
+// @version      0.3.3
+// @description  HighLights people whom you are following
+// @author       agropav
+// @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @require      https://raw.githubusercontent.com/agropav/hunterhelper-userscript/master/public/hunterhelper.min.js
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+;
