3817b14d35db71f0ce735249d18b04f8b13f1f85
Pavlik <raventomsk@mail2000.ru>
2022-07-28 20:20:05 +0700

    + hosts , +updateUrl

--- public/hunterhelper.userscript.js
+++ public/hunterhelper.userscript.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Twitter Following HighLight
 // @namespace    http://tampermonkey.net/
-// @version      0.3.3
+// @version      0.3.4
 // @description  HighLights people whom you are following
 // @author       agropav
 // @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @updateURL    https://raw.githubusercontent.com/agropav/hunterhelper-userscript/master/public/hunterhelper.userscript.js
+// @downloadURL  https://raw.githubusercontent.com/agropav/hunterhelper-userscript/master/public/hunterhelper.userscript.js
 // @require      https://raw.githubusercontent.com/agropav/hunterhelper-userscript/master/public/hunterhelper.min.js
 // @grant        none
 // @run-at       document-body
